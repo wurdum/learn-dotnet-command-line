ab41bc227762b4a95b10a297a83ca4376eeb3bec
Pavlo Rytikov <pavel.ritikov@gmail.com>
2025-01-03 21:57:57 +0200

    Init repo

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System.CommandLine;
+using Microsoft.Extensions.Configuration;
+
+var configuration = new ConfigurationBuilder()
+    .AddEnvironmentVariables()
+    .Build();
+
+var parameterOne = new Option<string?>(["-o", "--one"], () => null, "Parameter one");
+parameterOne.AddValidator(r =>
+{
+    var value = r.GetValueOrDefault<string?>();
+    if (value != null && value != "one")
+    {
+        r.ErrorMessage = "Parameter one must be 'one'";
+    }
+});
+
+var parameterTwo = new Option<string?>(["-t", "--two"], () => null, "Parameter two");
+
+var rootCommand = new RootCommand
+{
+    parameterOne,
+    parameterTwo
+};
+
+rootCommand.SetHandler(RunAsync, parameterOne, parameterTwo);
+await rootCommand.InvokeAsync(args);
+
+async Task RunAsync(string? one, string? two)
+{
+    Console.WriteLine($"Parameter one: {one}");
+    Console.WriteLine($"Parameter two: {two}");
+    Console.WriteLine($"Host: {configuration["HOST"]}");
+
+    await Task.Delay(1000);
+
+    Console.WriteLine("Done");
+}
